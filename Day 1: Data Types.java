d42f5a3585da4dc3bbc211d0eb899a2c11364708
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-04-27 20:39:58 +0530

    Create Day 1: Data Types.java

---
+++ Day 1: Data Types.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String str = sc.nextLine();
+        
+        System.out.println(n+4);
+        System.out.println(d+4);
+        System.out.println("HackerRank" + " " + str );
+    }
+}
