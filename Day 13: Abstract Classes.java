1e7dc5afffc8e1f89a505c63f5c57b950c99b508
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-05-09 11:45:55 +0530

    Day 13: Abstract Classes.java

---
+++ Day 13: Abstract Classes.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String title = sc.nextLine();
+        String author = sc.nextLine();
+        int price = sc.nextInt();
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
